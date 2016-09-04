2bb4a141b65f62e63be14fe577beca2afd72d3bb
Stephen Finucane <stephen.finucane@intel.com>
2016-09-04 01:12:38 +0100

    parsearchive: Convert to a management command
    As with parsemail, parsearchive makes more sense as a management
    command. Make it so.
    
    As with the conversion of the 'parsemail' tool, this removes
    customisable logging as it's not necessary.
    
    Signed-off-by: Stephen Finucane <stephen.finucane@intel.com>
    Closes-bug: #17

--- patchwork/tests/test_management.py
+++ patchwork/tests/test_management.py
@@ -49,3 +49,22 @@ class ParsemailTest(TestCase):
                              stdin=file_)
 
             self.assertEqual(exc.exception.code, 1)
+
+
+class ParsearchiveTest(TestCase):
+    def test_invalid_path(self):
+        out = StringIO()
+        with self.assertRaises(SystemExit) as exc:
+            call_command('parsearchive', 'xyz123random', stdout=out)
+        self.assertEqual(exc.exception.code, 1)
+
+    def test_invalid_mbox(self):
+        out = StringIO()
+        # we haven't created a project yet, so this will fail
+        call_command('parsearchive',
+                     os.path.join(TEST_MAIL_DIR,
+                                  '0001-git-pull-request.mbox'),
+                     stdout=out)
+
+        self.assertIn('Processed 1 messages -->', out.getvalue())
+        self.assertIn('  1 dropped', out.getvalue())
